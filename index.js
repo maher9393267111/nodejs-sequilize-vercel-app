874fc1c78cb9a3bfe02bdcb8198f0ffdc4b0e595
maher <gomemahero.com>
2022-10-26 22:01:53 +0300

    redeppp

--- index.js
+++ index.js
@@ -24,11 +24,19 @@
 import express from 'express';
 import cors from 'cors';
 import ProductRoute from './routes/ProductRoute.js';
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.json({ extended: false }));
 app.use(ProductRoute);
+
+app.use("/",  (req, res) => {
+    res.send("Hello World");
+} );
+
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log('Server running on port 5000'));
